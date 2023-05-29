c3b104e58e7b518bd4d36871d001a240a89cbeb8
kshtij sharma <kshitij593@gmail.com>
2023-05-29 21:40:49 +0530

    Add files via upload

---
+++ Hotel_Management_system/Global.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+rmno="101-106"
+rmno1="201-206"
+rmno2="301-306"
+prm='950'
+prm1='1600'
+prm2='3500'
+extpers='350' ####extra person rent
+hotel ='BLUECIRCLE'
+address='NEW JERSEY STATE U.S.A'
+phone_numb='+4910000'
+email='Bluecircle@hotmail.com'
+NAME='hello'
+PASS='123'
+NAME1='admin'
+PASS1='456'
+admin='admin'
+
+
+# In[ ]:
+
+
+
+
